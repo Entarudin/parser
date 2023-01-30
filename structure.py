9a4adaee489ab4900e981186c209ab762e1b1cbe
Entarudin <riazanov@sfedu.ru>
2023-01-30 21:28:43 +0300

    added Structure with cached_property flow

--- structure.py
+++ structure.py
@@ -1,3 +1,4 @@
+from functools import cached_property
 from translators import ListTranslator, ThreatTranslator, StatisticsThreatsTranslator
 from services import ConfigService
 from repositories import ThreatsRepository
@@ -12,3 +13,35 @@ threats_repository = ThreatsRepository(threats_translator, statistics_threats_tr
 config_service = ConfigService()
 
 matplotlib_wrapper = MatplotlibWrapper()
+
+
+class Structure:
+    @cached_property
+    def threat_translator(self):
+        return ThreatTranslator()
+
+    @cached_property
+    def threats_translator(self):
+        return ListTranslator(self.threat_translator)
+
+    @cached_property
+    def statistics_threats_translator(self):
+        return StatisticsThreatsTranslator()
+
+    @cached_property
+    def threats_repository(self):
+        return ThreatsRepository(
+            self.threats_translator,
+            self.statistics_threats_translator
+        )
+
+    @cached_property
+    def config_service(self):
+        return ConfigService()
+
+    @cached_property
+    def matplotlib_wrapper(self):
+        return MatplotlibWrapper()
+
+
+structure = Structure()
