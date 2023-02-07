7fd3499a35fb9c7bdf6116429800ab4b4c80baf1
Entarudin <riazanov@sfedu.ru>
2023-02-07 22:06:23 +0300

    rename fields feature on db ids

--- models/threats.py
+++ models/threats.py
@@ -7,4 +7,4 @@ class Threat:
         self.type: Optional[str] = None
         self.date_publication: Optional[str] = None
         self.description: Optional[str] = None
-        self.feature: Optional[str] = None
+        self.database_identifiers: Optional[list[str]] = None
